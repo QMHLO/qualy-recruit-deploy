bd3b8c4ff9387d5787d2c4d18b96a9194c90c55e
QMHLO <htetlinoo@qualymm.com>
2024-03-13 13:26:02 +0630

    added seo

--- src/components/JobForm.jsx
+++ src/components/JobForm.jsx
@@ -12,9 +12,11 @@ function JobForm({ closeForm }) {
           <div className="top">
             <h3>Apply For a job</h3>
             <p>
-              Job applicants will be responded to by email.
+              Qualy Myanmar is an offshore company based in Myanmar that specializes in Quality Assurance testing (Website verification, App verification, Software verification, Game Debugging), Web Design Coding, and Business Process Outsourcing (BPO) including administrative and operational tasks.
               <br />
-              Thank you.
+              Additionally, we support to Myanmar people who have the ambition to work in Japan as Training worker or Tokuteiginou worker.
+              <br />
+              Job offer for those who are interested in working at Japan IT company in Mawlamyine, Myanmar. Positions include Web Design Coder, Quality Assurance Engineer, Japanese Teacher, and more. Please apply your CV.
             </p>
             <p className="notice-txt">* MANDATORY FIELDS</p>
           </div>
